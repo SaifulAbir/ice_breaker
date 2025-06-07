1e67cec68f04cb6a92538a14a8cdf33a70d39aeb
Saiful Islam <saiful.abir20@gmail.com>
2025-06-07 08:48:35 +0200

    fullstack functionality added

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,15 +2,16 @@ from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from typing import Tuple
 
 # from langchain_anthropic import ChatAnthropic
 # from langchain_openai import ChatOpenAI
 # from langchain_ollama import ChatOllama
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_url = linkedin_lookup_agent(name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
 
@@ -34,9 +35,9 @@ def ice_break_with(name: str) -> str:
 
     chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data})
+    res: Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("photoUrl")
 
 if __name__ == "__main__":
     load_dotenv()
